9418eafa40964480fc77c35b3ec940bd4f9cdf3d
Slava Zakariev <124432505+SlavaZakariev@users.noreply.github.com>
2024-03-16 13:27:12 +0300

    Update count-vm.tf

--- terraform/17.3_constructions/git/count-vm.tf
+++ terraform/17.3_constructions/git/count-vm.tf
@@ -1,27 +1,28 @@
 data "yandex_compute_image" "ubuntu1" {
-  family = "ubuntu-2004-lts"
+  family = var.vm_ubuntu_ver
 }
 
 resource "yandex_compute_instance" "count" {
-    count = 2
-    name = "web-${count.index+1}"
-    platform_id = "standard-v1"
-    resources {
-      cores         = 2
-      memory        = 1
-      core_fraction = 5
-    }
+  count = 2
+  name = "web-${count.index+1}"
+  platform_id = var.vm_cpu_id
+  zone        = var.default_zone
+  resources {
+    cores         = var.vms_resources.vm_count_resources.cores
+    memory        = var.vms_resources.vm_count_resources.memory
+    core_fraction = var.vms_resources.vm_count_resources.core_fraction
+  }
     
-    boot_disk {
-      initialize_params {
-        image_id = data.yandex_compute_image.ubuntu1.image_id
-    }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu1.image_id
+   }
   }
 
   network_interface {
     security_group_ids = [yandex_vpc_security_group.example.id]    
-    subnet_id = yandex_vpc_subnet.develop.id
-    nat       = true
+    subnet_id          = yandex_vpc_subnet.develop.id
+    nat                = true
   }
 
   scheduling_policy {preemptible = true}
