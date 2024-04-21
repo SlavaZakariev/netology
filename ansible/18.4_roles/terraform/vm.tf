33ddcf57247d09b45e5069cba8882a756bfa790e
Slava Zakariev <124432505+SlavaZakariev@users.noreply.github.com>
2024-04-21 23:58:05 +0300

    Update vm.tf

--- ansible/18.4_roles/terraform/vm.tf
+++ ansible/18.4_roles/terraform/vm.tf
@@ -2,7 +2,6 @@
 data "yandex_compute_image" "ubuntu-clickhouse" {
   family = var.vm_os_ubuntu
 }
-
 resource "yandex_compute_instance" "clickhouse" {
   name        = var.vm_01
   hostname    = var.vm_01
@@ -12,38 +11,15 @@ resource "yandex_compute_instance" "clickhouse" {
     memory        = var.vms_resources.clickhouse.memory
     core_fraction = var.vms_resources.clickhouse.fraction
   }
-
   metadata = local.metadata
-
-#  metadata = {
-#    ssh-keys           = var.metadata.ssh-keys
-#    serial-port-enable = var.metadata.serial-port-enable
-#  }
-
-  #  metadata = {
-  #    user-data = templatefile("metadata.yaml", {
-  #      USER_NAME    = var.user_name
-  #      USER_SSH_KEY = var.user_ssh_key
-  #    })
-  #  }
-
-#  metadata = {
-#    user-data = "#cloud-config\nusers:\n  - name: <имя_пользователя>\n    groups: sudo\n    shell: /bin/bash\n    sudo: 'ALL=        (ALL) NOPASSWD:ALL'\n    ssh-authorized-keys:\n      - <содержимое_SSH-ключа>"
-#  }
-
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu-clickhouse.image_id
-#      name        = "disk-clickhouse-1"
-#      type        = "network-nvme"
-#      size        = "10"
     }
   }
-
   scheduling_policy {
     preemptible = true
   }
-
   network_interface {
     subnet_id  = yandex_vpc_subnet.subnet_db.id
     nat        = true
@@ -55,7 +31,6 @@ resource "yandex_compute_instance" "clickhouse" {
 data "yandex_compute_image" "ubuntu-vector" {
   family = var.vm_os_ubuntu
 }
-
 resource "yandex_compute_instance" "vector" {
   name        = var.vm_02
   hostname    = var.vm_02
@@ -65,20 +40,15 @@ resource "yandex_compute_instance" "vector" {
     memory        = var.vms_resources.vector.memory
     core_fraction = var.vms_resources.vector.fraction
   }
-
   metadata = local.metadata
-
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu-vector.image_id
-      #      disk_id = yandex_compute_disk.disk-vector.id
     }
   }
-
   scheduling_policy {
     preemptible = true
   }
-
   network_interface {
     subnet_id  = yandex_vpc_subnet.subnet_db.id
     nat        = true
@@ -90,7 +60,6 @@ resource "yandex_compute_instance" "vector" {
 data "yandex_compute_image" "ubuntu-lighthouse" {
   family = var.vm_os_ubuntu
 }
-
 resource "yandex_compute_instance" "lighthouse" {
   name        = var.vm_03
   hostname    = var.vm_03
@@ -100,20 +69,15 @@ resource "yandex_compute_instance" "lighthouse" {
     memory        = var.vms_resources.lighthouse.memory
     core_fraction = var.vms_resources.lighthouse.fraction
   }
-
   metadata = local.metadata
-
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu-lighthouse.image_id
-      #      disk_id = yandex_compute_disk.disk-lighthouse.id
     }
   }
-
   scheduling_policy {
     preemptible = true
   }
-
   network_interface {
     subnet_id  = yandex_vpc_subnet.subnet_db.id
     nat        = true
