fa8af9f3e8d95d5ee17fd9783feae62d1c6fd896
Slava Zakariev <124432505+SlavaZakariev@users.noreply.github.com>
2024-05-09 15:23:45 +0300

    Create vars.resources.tf

---
+++ ci-cd-devops/19.3_cicd/infrastructure/terraform/vars.resources.tf
@@ -0,0 +1,41 @@
+### vms_resources
+variable "vms_resources" {
+  type            = map(map(number))
+  default         = {
+    sonarqube = {
+      cores       = 2
+      memory      = 4
+      fraction    = 5
+    }
+    nexus     = {
+      cores       = 2
+      memory      = 4
+      fraction    = 5
+    }
+  }
+}
+
+variable "vm_os_centos" {
+  type        = string
+  default     = "centos-7"
+  description = "os version"
+}
+
+variable "vm_cpu_id_v1" {
+  type        = string
+  default     = "standard-v1"
+  description = "cpu id v1"
+}
+
+### vm_name
+variable "vm_01" {
+  type        = string
+  default     = "sonarqube"
+  description = "vm name"
+}
+
+variable "vm_02" {
+  type        = string
+  default     = "nexus"
+  description = "vm name"
+}
