4b750bfa827e13210f378369e8944a2d0fddaa70
Slava Zakariev <124432505+SlavaZakariev@users.noreply.github.com>
2024-05-26 13:26:52 +0300

    Create vars.provider.tf

---
+++ ci-cd-devops/19.4_jenkins/terraform/vars.provider.tf
@@ -0,0 +1,43 @@
+### cloud vars
+variable "token" {
+  type    = string
+  default = "y0_AgAAAAA*******"
+  # https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "cloud_id" {
+  type    = string
+  default = "b1gq1*******"
+  # https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type    = string
+  default = "b1grc*******"
+  # https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id
+}
+
+### cloud net
+variable "zone_a" {
+  type    = string
+  default = "ru-central1-a"
+  # https://cloud.yandex.ru/docs/overview/concepts/geo-scope
+}
+
+variable "cidr_a" {
+  type    = list(string)
+  default = ["10.10.10.0/24"]
+  # https://cloud.yandex.ru/docs/vpc/operations/subnet-create
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "vpc_ci-cd"
+  description = "VPC network for CI-CD VMs"
+}
+
+variable "subnet_name" {
+  type        = string
+  default     = "subnet_ci-cd"
+  description = "Subnet name for CI-CD VMs"
+}
