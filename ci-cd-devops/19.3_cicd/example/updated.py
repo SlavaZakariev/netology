d70c1b9fe197e5f43b8d27b9dc3ecda5dabc9d09
Slava Zakariev <124432505+SlavaZakariev@users.noreply.github.com>
2024-06-11 00:05:27 +0300

    Create updated.py

---
+++ ci-cd-devops/19.3_cicd/example/updated.py
@@ -0,0 +1,12 @@
+def increment(index):
+    index_new = index + 1
+    return index_new
+def get_square(numb):
+    return numb*numb
+def print_numb(numb):
+    print("Number is {}".format(numb))
+
+index = 0
+while (index < 10):
+    index = increment(index)
+    print(get_square(index))
